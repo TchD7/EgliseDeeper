c570a1750b94349ceb55056d722e43d67f46d65e
Tchondo <edisontchondo@gmail.com>
2021-12-06 08:59:03 +0000

    first

--- DeeperPro/settings.py
+++ DeeperPro/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#t@as(-253gg74vz4jcflm2#i$08w+cbr6@#$mquh7e(u@+91b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['deeperlife-togo.heroku.com']
 
